a0994230af14e47777f2cb9b6f77300819979cb5
Gervy De La Cruz <gervy_969g@hotmail.com>
2024-06-16 20:00:53 -0400

    Configure app for Heroku deployment

--- to-do-list-web-app.py
+++ to-do-list-web-app.py
@@ -54,4 +54,6 @@ def clear_tasks():
     conn.close()
     return redirect(url_for('index'))
 
-if __name_
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
