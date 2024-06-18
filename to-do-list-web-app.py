c0e0a922b6127a406085a0b9cfe94da2b97b910a
Gervy De La Cruz <gervy_969g@hotmail.com>
2024-06-18 15:59:19 -0400

    Fix path to styles.css and add background image

--- to-do-list-web-app.py
+++ to-do-list-web-app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
-import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 # Initialize the database
 def initialize_database():
@@ -55,5 +53,4 @@ def clear_tasks():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
